000da89f063e87ee996356a7fbebac73ffdbcfd8
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-09-09 18:08:32 -0400

    Made it through step 5.4 of tutorisl

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float moveX, moveY;
+    public float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        rb.AddForce(movement * speed);
+    }
+
+
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private Vector3 rotation;
+
+    void Start()
+    {
+        rotation = new Vector3(15, 30, 45);
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(rotation * Time.deltaTime);
+        
+    }
+}
